1df3f08ec198ba30db2ba4a56fbbce5334cb299d
jeffdoeswork <jeff@does.work>
2022-12-13 09:19:36 -0500

    register form works

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -15,13 +15,13 @@ class NoteSerializer(ModelSerializer):
 class RegisterSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','username','password','first_name', 'last_name')
+        fields = ('id','username','password','first_name', 'last_name', 'email')
         extra_kwargs = {
             'password':{'write_only': True},
         }
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], password = validated_data['password'], first_name=validated_data['first_name'],  last_name=validated_data['last_name'])
+        user = User.objects.create_user(validated_data['username'], email = validated_data['email'], password = validated_data['password'], first_name=validated_data['first_name'],  last_name=validated_data['last_name'])
         return user
 
 # User serializer
