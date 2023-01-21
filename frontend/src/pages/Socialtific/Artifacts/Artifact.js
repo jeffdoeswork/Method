fccf8e5962148fd1f9c6f5337bb5535d51df1121
jeffdoeswork <jeff@does.work>
2023-01-20 20:10:08 -0500

    swiper takes child

--- frontend/src/pages/Socialtific/Artifacts/Artifact.js
+++ frontend/src/pages/Socialtific/Artifacts/Artifact.js
@@ -9,69 +9,44 @@ import Col from 'react-bootstrap/Col';
 import profile_pic from './profile_pic.PNG';
 
 function Artifact(props) {
-    const [getdata, setGetdata] = useState([
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-        {"created_at" : "10/23/2022 8:15PM", "email_datas" : "Jeff@does.work", "datas" : "ThThe dog of wisof wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog of wisdom: If a ball is toThe dog dom: If a ball is toThe dog of wisdom: If a ball is toe dog of wisdom: If a ball is too big for your mouth, it is not yours", "id" : 1},
-    ]);
-
-    useEffect(()=> {
-        console.log(getdata);
-    }, [])
 
     return (
-        
         <div>
-
-            {getdata.map((data) => (
-                <div>
             <Row className="d-flex justify-content-center">
                 <Col xs={12} sm={8} md={7} xl={6}>
-            <Container className="data_artifact">
-                
-                <Row xs={3} sm={3}>
-                    <Col sm={2}>
-                    <Image src={profile_pic} roundedCircle fluid/>                   
-                    </Col>
-                    <Col sm={5}>
-                        <p>{data.email_datas}</p>    
-                        <p>{data.created_at}</p>                 
-                    </Col>
-                    <Col sm={5} className="d-flex justify-content-end">
-                        <p>Link To Method / Meta Method</p>                     
-                    </Col>
-                </Row>
-                <Row>
-                    <p key={data.id}>
-                    {data.datas}
-                    </p>
-                </Row>
-                <Row>
-                <Col sm={1} className="d-flex justify-content-end"> 
-                         Like             
+                    <Container className="data_artifact">
+                        <Row xs={3} sm={3}>
+                            <Col sm={2}>
+                            <Image src={profile_pic} roundedCircle fluid/>                   
+                            </Col>
+                            <Col sm={5}>
+                                <p>{props.data.email_datas}</p>    
+                                <p>{props.data.created_at}</p>                 
+                            </Col>
+                            <Col sm={5} className="d-flex justify-content-end">
+                                <p>Link To Method / Meta Method</p>                     
+                            </Col>
+                        </Row>
+                        <Row>
+                            <p key={props.data.id}>
+                            {props.data.datas}
+                            </p>
+                        </Row>
+                        <Row>
+                        <Col sm={1} className="d-flex justify-content-end"> 
+                                    Like             
+                        </Col>
+                        <Col sm={2}>
+                                    Mass             
+                        </Col>
+                        <Col sm={8} className="d-flex justify-content-end">
+                                ^ vote ^             
+                        </Col>
+                        </Row>
+                    </Container>
                 </Col>
-                <Col sm={2}>
-                         Mass             
-                </Col>
-                <Col sm={8} className="d-flex justify-content-end">
-                       ^ vote ^             
-                </Col>
-
-                </Row>
-
-            </Container>
-            </Col>
             </Row>
-            {/* <br></br> */}
-            </div>
-            )
-        )}
+            <br></br>
         </div>
   )
 };
--- frontend/src/pages/Socialtific/Methodmaker/ArtifactSlider.js
+++ frontend/src/pages/Socialtific/Methodmaker/ArtifactSlider.js
@@ -7,56 +7,16 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import profile_pic from './profile_pic.PNG';
+import Artifact from '../Artifacts/Artifact';
 
 function ArtifactSlider(props) {
 
-    return (
-        
+    return ( 
         <div>
-
             {props.getdata.map((data) => (
-                <div>
-            <Row className="d-flex justify-content-center">
-                <Col xs={12} sm={8} md={7} xl={6}>
-            <Container className="data_artifact">
-                
-                <Row xs={3} sm={3}>
-                    <Col sm={2}>
-                    <Image src={profile_pic} roundedCircle fluid/>                   
-                    </Col>
-                    <Col sm={5}>
-                        <p>{data.email_datas}</p>    
-                        <p>{data.created_at}</p>                 
-                    </Col>
-                    <Col sm={5} className="d-flex justify-content-end">
-                        <p>Link To Method / Meta Method</p>                     
-                    </Col>
-                </Row>
-                <Row>
-                    <p key={data.id}>
-                    {data.datas}
-                    </p>
-                </Row>
-                <Row>
-                <Col sm={1} className="d-flex justify-content-end"> 
-                         Like             
-                </Col>
-                <Col sm={2}>
-                         Mass             
-                </Col>
-                <Col sm={8} className="d-flex justify-content-end">
-                       ^ vote ^             
-                </Col>
-
-                </Row>
-
-            </Container>
-            </Col>
-            </Row>
-            {/* <br></br> */}
-            </div>
-            )
-        )}
+                <Artifact data={data}/>
+                )
+            )}
         </div>
   )
 };
