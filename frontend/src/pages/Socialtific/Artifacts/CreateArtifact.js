c0c438181f826b67bd2daf8be92cb825245e65e6
jeffdoeswork <jeff@does.work>
2023-01-23 21:53:39 -0500

    toggle

---
+++ frontend/src/pages/Socialtific/Artifacts/CreateArtifact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateArtifact = () => {
+  return (
+    <div>
+        <h1>CreateArtifact</h1>
+    </div>
+  )
+}
+
+export default CreateArtifact
--- frontend/src/pages/Socialtific/Methodmaker/ArtifactSlider.js
+++ frontend/src/pages/Socialtific/Methodmaker/ArtifactSlider.js
@@ -8,42 +8,61 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import profile_pic from './profile_pic.PNG';
 import Artifact from '../Artifacts/Artifact';
+import CreateArtifact from '../Artifacts/CreateArtifact';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
 // https://www.youtube.com/watch?v=aiedzAHmVII
 function ArtifactSlider(props) {
     const [index, setIndex] = useState(0);
+    const [makeartifact, setMakeArtifact] = useState(true);
+
     const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
     };
 
     return ( 
         <div>
-        <Carousel 
-        indicators={false}
-        interval={null} 
-        variant="dark" 
-        activeIndex={index} 
-        onSelect={handleSelect}>
+        { makeartifact ? 
+        <div>
+            <Carousel 
+            indicators={false}
+            interval={null} 
+            variant="dark" 
+            activeIndex={index} 
+            onSelect={handleSelect}>
 
-            {props.getdata.map((data) => (
-                <Carousel.Item>
-                    <Artifact data={data}/>
-                </Carousel.Item>
-                )
-            )}
-        </Carousel>
-        <div className="d-flex justify-content-center mt-3">
-            {props.getdata.map((item, i) => (
-                <button
-                key={i}
-                className={`btn btn-primary mx-1 ${index === i ? 'active' : ''}`}
-                onClick={() => handleSelect(i)}
-                >
-                {i+1}
-                </button>
-            ))}
+                {props.getdata.map((data) => (
+                    <Carousel.Item>
+                        <Artifact data={data}/>
+                    </Carousel.Item>
+                    )
+                )}
+            </Carousel>
+            <div className="d-flex justify-content-center mt-3">
+                {props.getdata.map((item, i) => (
+                    <button
+                    key={i} b
+                    className={`btn btn-primary mx-1 ${index === i ? 'active' : ''}`}
+                    onClick={() => handleSelect(i)}
+                    >
+                    {i+1}
+                    </button>
+                ))}
+            </div>
+            <Button onClick={() => setMakeArtifact(false)}> Make  </Button>
+
         </div>
+        :
+        <div>
+            <CreateArtifact />
+
+            <Button onClick={() => setMakeArtifact(true)}> Browse  </Button>
+                 
+
+        </div>
+
+            }
+
         </div>
   )
 };
