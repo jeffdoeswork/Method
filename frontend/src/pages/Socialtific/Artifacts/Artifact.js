e1839852f6b348287c4e44ebab4b2f7562c06a5b
jeffdoeswork <jeff@does.work>
2023-05-21 07:13:45 -0400

    artifacts are fine

--- frontend/src/pages/Socialtific/Artifacts/Artifact.js
+++ frontend/src/pages/Socialtific/Artifacts/Artifact.js
@@ -17,26 +17,30 @@ function Artifact(props) {
     return (
         <div>   
 <Row className="d-flex justify-content-center">
-    <h2>{ idMap }</h2>
-    <Col xs={12} sm={8} md={7} xl={6}>
+    
+    <Col xs={12} sm={12} md={10} xl={9}>
         <Container className={props.artifact_className}>
             <Row>
-                <Col xs={3} md={2} lg={2}>
+                <Col xs={2} md={2} lg={2}>
                     <Image src={profile_pic} roundedCircle style={{ width: "80px", height: "80px" }} />
                 </Col>
-                <Col xs={3} md={2} lg={2}>
+                <Col xs={2} md={2} lg={2}>
                     <p className="d-flex justify-content-start">{props.data.user.username}</p>
                 </Col>
                 <Col xs={6} md={8} lg={8}>
                     <p className="d-flex justify-content-end"> {props.data.created_at}</p>
                 </Col>
             </Row>
-            <Row>
-            <Col xs={4} md={2}> 
+            <Row style={{ paddingBottom: '10px' }}>
+            <Col xs={4} md={2} className="d-flex flex-column justify-content-end">
+            <h3> #{ idMap }</h3>
             </Col>
-                <Col xs={8}>
+                <Col xs={8} className="maxHeight">
                     <p>{props.data.description}</p>
                 </Col>
+                <Col xs={2} md={2} className="d-flex flex-column justify-content-end align-items-end">
+                <p> Likes </p>
+            </Col>
             </Row>
         </Container>
     </Col>
